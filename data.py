f5027d0a108e05c313f95f00c4c59143464ecca2
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-01 18:36:14 +0000

    add basic app function

---
+++ data.py
@@ -0,0 +1,40 @@
+def Articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article one",
+            "body": ''' Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit.
+            Explicabo dolor nisi veritatis
+            recusandae sapiente nam praesentium quod
+            corporis iusto! Magni, deleniti.
+             Esse et atque autem nisi ducimus eveniet asperiores ad?''',
+            "author": "Brad Traversy",
+            "create_date": "04-25-2017"
+        },
+        {
+            "id": 2,
+            "title": "Article two",
+            "body": ''' Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit.
+            Explicabo dolor nisi veritatis
+            recusandae sapiente nam praesentium quod
+            corporis iusto! Magni, deleniti.
+             Esse et atque autem nisi ducimus eveniet asperiores ad?''',
+            "author": "John Doe",
+            "create_date": "04-25-2017"
+        },
+        {
+            "id": 3,
+            "title": "Article Three",
+            "body": ''' Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit.
+            Explicabo dolor nisi veritatis
+            recusandae sapiente nam praesentium quod
+            corporis iusto! Magni, deleniti.
+             Esse et atque autem nisi ducimus eveniet asperiores ad?''',
+            "author": "Brad Traversy",
+            "create_date": "04-25-2017"
+        }
+    ]
+    return articles
