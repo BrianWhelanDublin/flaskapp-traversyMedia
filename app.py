9f7ca78168fadbf2ec68eda4de93a9c070ea09a6
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-02 16:44:32 +0000

    add function to add articles

--- app.py
+++ app.py
@@ -41,12 +41,24 @@ def about():
 
 @app.route("/articles")
 def articles():
-    return render_template("articles.html", articles=Articles)
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    if result > 0:
+        return render_template("articles.html", articles=articles)
+    else:
+        msg = "No Articles Found"
+        return render_template("articles.html", msg=msg)
+    cur.close()
 
 
 @app.route("/article/<string:id>")
 def article(id):
-    return render_template("article.html", id=id)
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles WHERE id = %s",
+                         [id])
+    article = cur.fetchone()
+    return render_template("article.html", article=article)
 
 
 class RegisterForm(Form):
@@ -144,7 +156,44 @@ def logout():
 @app.route("/dashboard")
 @is_logged_in
 def dashboard():
-    return render_template("dashboard.html")
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    if result > 0:
+        return render_template("dashboard.html", articles=articles)
+    else:
+        msg = "No Articles Found"
+        return render_template("dashboard.html", msg=msg)
+    cur.close()
+
+
+class ArticleForm(Form):
+    title = StringField("Title", [validators.Length(min=1, max=200)])
+    body = TextAreaField("Body", [validators.Length(min=30)])
+
+
+@app.route("/add_article", methods=["GET", "POST"])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == "POST" and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO articles(title, body, author) \
+                    VALUES(%s, %s, %s)", (title, body, session["username"]))
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash("Article created", "success")
+
+        return redirect(url_for("dashboard"))
+
+    return render_template("add_article.html", form=form)
 
 
 if __name__ == "__main__":
