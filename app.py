676ceb1a0ef72ceee18292f738e7e3ca4a85d1f9
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-02 13:12:54 +0000

    add log in and out logic

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from wtforms import (Form, StringField,
                      TextAreaField, PasswordField,
                      validators)
 from passlib.hash import sha256_crypt
+from functools import wraps
+
 if os.path.exists("env.py"):
     import env
 
@@ -19,7 +21,7 @@ app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "myflaskapp"
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MySQL
 mysql = MySQL(app)
 
@@ -84,8 +86,69 @@ def register():
     return render_template("/register.html", form=form)
 
 
+# user login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # get form fields
+        username = request.form["username"]
+        password_candidate = request.form["password"]
+
+        # create cursor
+        cur = mysql.connection.cursor()
+
+        # get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s",
+                             [username])
+
+        if result > 0:
+            # get stored has
+            data = cur.fetchone()
+            password = data["password"]
+            # compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                session["logged_in"] = True
+                session["username"] = username
+                flash("You are now logged in", "success")
+                return redirect(url_for("dashboard"))
+            else:
+                error = "Invalid Login"
+                return render_template("login.html", error=error)
+            cur.close()
+        else:
+            error = "Username not found"
+            return render_template("login.html", error=error)
+
+    return render_template("login.html")
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route("/logout")
+@is_logged_in
+def logout():
+    session.clear()
+    flash("You are now logged out", "success")
+    return redirect(url_for("login"))
+
+
+@app.route("/dashboard")
+@is_logged_in
+def dashboard():
+    return render_template("dashboard.html")
+
+
 if __name__ == "__main__":
-    app.secret_key="secret1234"
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+    app.secret_key = "secret1234"
+    app.run(host=os.getenv("IP"),
+            port=int(os.getenv("PORT")),
             debug=True)
